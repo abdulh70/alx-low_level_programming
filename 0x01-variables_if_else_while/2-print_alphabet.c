9a6f630d66c180089d61839aab59634dd8a64472
Abdulaziz Hassan <abdulazizh70@gmail.com>
2022-12-09 18:31:32 -0800

    added file

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main -prints alphabet lower case
+ * Return: Always 0 success
+ */
+int main(void)
+{
+char alpha;
+for (alpha = 'a'; alpha <= 'z'; alpha++)
+{
+putchar(alpha);
+
+}
+putchar('\n');
+return (0);
+
+
+
+
+}
