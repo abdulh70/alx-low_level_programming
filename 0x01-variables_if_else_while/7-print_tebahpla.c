affb0f530df183383a8acdded8ed22ec8042c06f
Abdulaziz Hassan <abdulazizh70@gmail.com>
2022-12-11 12:11:22 -0800

    addede file

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main -prints lowercase alphabet in reverse
+ * Return: Always (0)
+ */
+int main(void)
+{
+char alpha;
+for (alpha = 'z'; alpha >= 'a'; alpha++)
+{
+putchar(alpha);
+}
+putchar('\n');
+
+
+}
