9599c43fd3f0d59b22299e0c88dc8a8553697dc5
Abdulaziz Hassan <abdulazizh70@gmail.com>
2022-12-11 11:34:39 -0800

    added number

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main -Entry point
+ * Return: Always (0)
+ */
+int main(void)
+{
+int m;
+for (m = 48; m < 58; m++)
+{
+	putchar (m);
+}
+putchar ('\n');
+return (0);
+
+
+
+}
