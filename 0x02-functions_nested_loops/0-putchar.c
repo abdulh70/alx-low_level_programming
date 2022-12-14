2ac04e510cf807414dd56137e22f450a99fe7f82
Abdulaziz Hassan <abdulazizh70@gmail.com>
2022-12-14 14:02:14 -0800

    added file

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main -Entry point
+ * Return: Always (0)
+ */
+int main(void)
+{
+int putcharr;
+putcharr = '_';
+putchar(putcharr);
+putcharr = 'p';
+putchar(putcharr);
+putcharr = 'u';
+putchar(putcharr);
+putcharr = 't';
+putchar(putcharr);
+putcharr = 'c';
+putchar(putcharr);
+putcharr = 'h';
+putchar(putcharr);
+putcharr = 'a';
+putchar(putcharr);
+putcharr = 'r';
+putchar(putcharr);
+
+return (0);
+
+}
