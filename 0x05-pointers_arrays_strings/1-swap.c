f012ba208bc209b9d877538624286b5b5e2a456d
Abdulaziz Hassan <abdulazizh70@gmail.com>
2022-12-19 15:50:51 -0800

    added file

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,9 +1,9 @@
 /**
  * swap_int -a program that swaps values
  * of two integer
- *  @a: input parameter 
- *   @b: input parameter 
- *   Return: zero
+ * @a: input parameter
+ * @b: input parameter
+ * Return: zero
  */
 void swap_int(int *a, int *b);
 {
