c11b9abf75a48609ea38aff84dbcf955f686c462
Abdulaziz Hassan <abdulazizh70@gmail.com>
2022-12-19 15:17:41 -0800

    added file

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+/**
+ * reset_to_98 -program that takes a pointer
+ * as an argument and updates the value
+ * to 98
+ *  @n: int parameter
+ * Return: zero
+ */
+void reset_to_98(int *n);
+{
+*n=98;
+
+}
