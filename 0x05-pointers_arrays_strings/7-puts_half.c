d18332e339335011c32a21830e89b536c241732e
Abdulaziz Hassan <abdulazizh70@gmail.com>
2022-12-19 17:37:56 -0800

    added file

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,24 @@
+#include"main.h"
+/**
+ * puts_half - prints half of a string
+ * @str: parameter input
+ * Return: zero
+ */
+void puts_half(char *str)
+{
+int k, m;
+for (k = 0; str[k] != '\0'; ++k)
+;
+if (k % 2 == 0)
+{
+for (m = l / 2; str[m] != '\0'; ++m)
+_putchar(str[m]);
+}
+else
+{
+for (m = ((k - 1) / 2) + 1; str[m] != '\0'; ++m)
+_putchar(str[m]);
+}
+_putchar('\n');
+}
+
