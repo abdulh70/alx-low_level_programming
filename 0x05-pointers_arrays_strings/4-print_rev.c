2131e7a62a0294b3219e9c459c66e1731915435d
Abdulaziz Hassan <abdulazizh70@gmail.com>
2022-12-19 16:57:32 -0800

    added file

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -7,10 +7,13 @@
 void print_rev(char *s)
 {
 int count;
+/*finds the length of string */
+
 for (count = 0; s[count] != '\0'; ++count)
 {
 	;
 
+/*print char from the last index as you decrement*/
 for (--count; count >= 0; --count)
 {
 _putchar(s[count]);
