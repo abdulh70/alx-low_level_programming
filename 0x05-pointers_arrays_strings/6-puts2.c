d1842904327d1c212355469d4a93a6ca6a86c6c7
Abdulaziz Hassan <abdulazizh70@gmail.com>
2022-12-19 17:19:23 -0800

    added file

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,16 @@
+#include"main.h"
+/**
+ * puts2 - prints every other character of a string
+ * @str: string parameter input
+ * Return: zero
+ */
+void puts2(char *str)
+{
+int k;
+for (k = 0; str[k] != '\0'; ++k)
+{
+if (k % 2 == 0)
+_putchar(str[k]);
+}
+_putchar('\n');
+}
