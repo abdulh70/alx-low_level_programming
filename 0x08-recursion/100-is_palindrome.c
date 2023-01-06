b33d130ed431d91740d87731ceade763eaafe1c5
Abdulaziz Hassan <abdulazizh70@gmail.com>
2023-01-06 15:24:14 -0800

    added file

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,19 +1,41 @@
 #include "main.h"
-int find_strlen(char *s);
-int check_palindrome(char *s, int len, int index);
-int is_palindrome(char *s);
 /**
-* find_strlen - Returns the length of a string.
-* * @s: string
-* Return: the length of a string.
+* _strlen_recursion -function returns length of a string.
+* @s: string parameter
+* Return: length of a string.
 */
-int find_strlen(char *s)
+int _strlen_recursion(char *s)
 {
-int len = 0;
-if (*(s + len)}
-{
-len++;
-len += find_strlen(s + len);
+if (*s == '\0')
+return (0);
+else
+return (1 + _strlen_recursion(s + 1));
 }
-return (len);
+/**
+* operator - compares each character of the string.
+* @s: string
+* @n1: smallest iterator.
+* @n2: largest iterator.
+* Return: .
+*/
+int operator(char *s, int n1, int n2)
+{
+if (*(s + n1) == *(s + n2))
+{
+if (n1 == n2 || n1 == n2 + 1)
+return (1);
+return (0 + operator(s, n1 + 1, n2 - 1));
+}
+return (0);
+}
+/**
+* is_palindrome - checks if a string is a palindrome.
+* @s: string parameter.
+* Return: 1 if s is a palindrome, 0 if not.
+*/
+int is_palindrome(char *s)
+{
+if (*s == '\0')
+return (1);
+return (operator(s, 0, _strlen_recursion(s) - 1));
 }
