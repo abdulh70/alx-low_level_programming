b19d74d5fee3a3a4eaec899712518696c701ff83
Abdulaziz Hassan <abdulazizh70@gmail.com>
2023-01-13 18:15:45 -0800

    added file

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,3 +1,4 @@
+#ifndef MAIN_H
 #define MAIN_H
 #include <stdio.h>
 #include <stdlib.h>
@@ -9,5 +10,4 @@ void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 #endif
-
 
